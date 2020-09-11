e809c70185d65e1e97680fcf758ed964559db9d1
coderealistics <coderealistics@yandex.com>
2020-09-11 11:44:40 +0500

    Added React-Router-DOM

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Amazon-Clone</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <h1></h1>
+      </div>
+    </Router>
   );
 }
 
